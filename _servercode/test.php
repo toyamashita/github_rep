d66a74d498bf830f7efa1f77b74fef4f957f6bf7
toyamashita <toshichika.yama@gmail.com>
2013-11-19 07:39:01 +0900

    test comment added

--- _servercode/test.php
+++ _servercode/test.php
@@ -1,9 +1,9 @@
 <?php
-
+	//test comment2
 	$time1 = "2013/11/04";
 	$time2 = date("Y/m/d");
 	
-	//日付をstrtotime()で変換して比較します
+	//日付をstrtotime()で変換して比較します。
 	if (strtotime($time1) > strtotime($time2)) {
 		echo $time1."is newer than ".$time2;
 	} else {
